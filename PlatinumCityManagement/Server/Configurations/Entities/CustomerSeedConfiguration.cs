f63db96ffd19c20520fb3d38fe9b0393d3686e8a
JUSTA THEI NADI AUNG 2203032D <2203032D@student.tp.edu.sg>
2024-01-30 14:17:02 +0800

    CRUD

---
+++ PlatinumCityManagement/Server/Configurations/Entities/CustomerSeedConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlatinumCityManagement.Shared.Domain;
+using System.Drawing;
+
+namespace PlatinumCityManagement.Server.Configurations.Entities
+{
+    public class CustomerSeedConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasData(
+                  new Customer
+                  {
+                      Id = 1,
+                      Name = "Johnny Lee",
+                      DOB = new DateTime(2005, 7, 15),
+                      Address = "707 Tampines Street 71 #09-20, 520707",
+                      EmailAddress = "lee@gmail.com",
+                      ContactNumber = "89220951",
+                      MembershipType = "Premium",
+                      Points = 133
+
+                  }
+
+
+
+);                  
+        }
+    }
+}
