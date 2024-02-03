7e156c1e33f1e0900eb439ffa6a40859a0620326
SESANE WIN 2200132E <2200132E@student.tp.edu.sg>
2024-02-03 16:49:01 +0800

    CRUD_sesane

---
+++ PlatinumCityManagement/Server/Configurations/Entities/HallSeedConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlatinumCityManagement.Shared.Domain;
+
+namespace PlatinumCityManagement.Server.Configurations.Entities
+{
+    public class HallSeedConfiguration : IEntityTypeConfiguration<Hall>
+    {
+        public void Configure(EntityTypeBuilder<Hall> builder)
+        {
+            builder.HasData(
+                new Hall
+                {
+                    Id = 1,
+                    Name = "1",
+                    ScreeningQuality = "IMAX",
+                    Capacity = 100,
+                    CinemaBranchId = 1,
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+            );
+        }
+    }
+}
---
+++ PlatinumCityManagement/Server/Controllers/SeatsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlatinumCityManagement.Server.Data;
+using PlatinumCityManagement.Shared.Domain;
+using PlatinumCityManagement.Server.IRepository;
+using PlatinumCityManagement.Server.Repository;
+
+namespace PlatinumCityManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatsController : ControllerBase
+    {
+        //refactored
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+        //refactored
+        //public SeatsController(ApplicationDbContext context)
+        public SeatsController(IUnitOfWork unitOfWork)
+        {
+            //refactored
+            //_context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Seats
+        [HttpGet]
+        //refactored
+        //public async Task<ActionResult<IEnumerable<Seat>>> GetSeats()
+        public async Task<IActionResult> GetSeats()
+        {
+            //refactored
+            //if (_context.Seats == null)
+            //{
+            //    return NotFound();
+            //}
+            //  return await _context.Seats.ToListAsync();
+            var seats = await _unitOfWork.Seats.GetAll(includes: q => q.Include(x => x.Hall));
+            return Ok(seats);
+        }
+
+        // GET: api/Seats/5
+        [HttpGet("{Id}")]
+        //refactored
+        //public async Task<ActionResult<Seat>> GetSeat(int Id)
+        public async Task<IActionResult> GetSeats(int Id)
+        {
+            /*if (_context.Seats == null)
+            {
+                return NotFound();
+            }*/
+            var seat = await _unitOfWork.Seats.Get(q => q.Id == Id);
+
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(seat);
+        }
+
+        // PUT: api/Seats/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkId=2123754
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> PutSeat(int Id, Seat seat)
+        {
+            if (Id != seat.Id)
+            {
+                return BadRequest();
+            }
+
+            //refactored
+            //_context.Entry(seat).State = EntityState.Modified;
+            _unitOfWork.Seats.Update(seat);
+
+            try
+            {
+                //refactored
+                //await _context.SaveChangesAsync();
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //refactored
+                //if (!SeatExists(Id))
+                if (!await SeatExists(Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Seats
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkId=2123754
+        [HttpPost]
+        public async Task<ActionResult<Seat>> PostSeat(Seat seat)
+        {
+            //refactored
+            /*if (_context.Seats == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Seats'  is null.");
+            }
+              _context.Seats.Add(seat);
+              await _context.SaveChangesAsync();*/
+            await _unitOfWork.Seats.Insert(seat);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetSeat", new { seat.Id }, seat);
+        }
+
+        // DELETE: api/Seats/5
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteSeat(int Id)
+        {
+            /*if (_context.Seats == null)
+            {
+                return NotFound();
+            }*/
+            //refactored
+            //var seat = await _context.Seats.FindAsync(Id);
+            var seat = await _unitOfWork.Seats.Get(q => q.Id == Id);
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            //refactored
+            //_context.Seats.Remove(seat);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Seats.Delete(Id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        //refactored
+        //private bool SeatExists(int Id)
+        private async Task<bool> SeatExists(int Id)
+        {
+            //refactored
+            //return (_context.Seats?.Any(e => e.Id == Id)).GetValueOrDefault();
+            var seat = await _unitOfWork.Seats.Get(q => q.Id == Id);
+            return seat != null;
+        }
+    }
+}
