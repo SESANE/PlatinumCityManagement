eb71378b4d43b10404607fe393a111c7863eb47a
SESANE WIN 2200132E <2200132E@student.tp.edu.sg>
2024-02-04 12:30:10 +0800

    Hall_changes

--- PlatinumCityManagement/Server/Configurations/Entities/CinemaBranchSeedConfiguration.cs
+++ PlatinumCityManagement/Server/Configurations/Entities/CinemaBranchSeedConfiguration.cs
@@ -4,23 +4,45 @@ using PlatinumCityManagement.Shared.Domain;
 
 namespace PlatinumCityManagement.Server.Configurations.Entities
 {
-    public class CinemaBranchSeedConfiguration : IEntityTypeConfiguration<CinemaBranch>
-    {
-        public void Configure(EntityTypeBuilder<CinemaBranch> builder)
-        {
-            builder.HasData(
-                new CinemaBranch
-                {
-                    Id = 1,
-                    Name = "Platinum City Tampines",
-                    Address = "Tampines",
-                    HallQuantity = 9,
-                    DateCreated = DateTime.Now,
-                    DateUpdated = DateTime.Now,
-                    CreatedBy = "System",
-                    UpdatedBy = "System"
-                }
-            );
-        }
-    }
+	public class CinemaBranchSeedConfiguration : IEntityTypeConfiguration<CinemaBranch>
+	{
+		public void Configure(EntityTypeBuilder<CinemaBranch> builder)
+		{
+			builder.HasData(
+				new CinemaBranch
+				{
+					Id = 1,
+					Name = "Platinum City Tampines",
+					Address = "Tampines",
+					HallQuantity = 9,
+					DateCreated = DateTime.Now,
+					DateUpdated = DateTime.Now,
+					CreatedBy = "System",
+					UpdatedBy = "System"
+				},
+				new CinemaBranch
+				{
+					Id = 2,
+					Name = "Platinum City Bedok",
+					Address = "Bedok",
+					HallQuantity = 10,
+					DateCreated = DateTime.Now,
+					DateUpdated = DateTime.Now,
+					CreatedBy = "System",
+					UpdatedBy = "System"
+				},
+				new CinemaBranch
+				{
+					Id = 3,
+					Name = "Platinum City Jewel",
+					Address = "Jewel Changi Airport",
+					HallQuantity = 8,
+					DateCreated = DateTime.Now,
+					DateUpdated = DateTime.Now,
+					CreatedBy = "System",
+					UpdatedBy = "System"
+				}
+			);
+		}
+	}
 }
--- PlatinumCityManagement/Server/Configurations/Entities/HallSeedConfiguration.cs
+++ PlatinumCityManagement/Server/Configurations/Entities/HallSeedConfiguration.cs
@@ -4,24 +4,50 @@ using PlatinumCityManagement.Shared.Domain;
 
 namespace PlatinumCityManagement.Server.Configurations.Entities
 {
-    public class HallSeedConfiguration : IEntityTypeConfiguration<Hall>
-    {
-        public void Configure(EntityTypeBuilder<Hall> builder)
-        {
-            builder.HasData(
-                new Hall
-                {
-                    Id = 1,
-                    Name = "1",
-                    ScreeningQuality = "IMAX",
-                    Capacity = 100,
-                    CinemaBranchId = 1,
-                    DateCreated = DateTime.Now,
-                    DateUpdated = DateTime.Now,
-                    CreatedBy = "System",
-                    UpdatedBy = "System"
-                }
-            );
-        }
-    }
+	public class HallSeedConfiguration : IEntityTypeConfiguration<Hall>
+	{
+		public void Configure(EntityTypeBuilder<Hall> builder)
+		{
+			builder.HasData(
+				new Hall
+				{
+					Id = 1,
+					Name = "1",
+					ScreeningQuality = "IMAX",
+					Capacity = 100,
+					CinemaBranchId = 1,
+					DateCreated = DateTime.Now,
+					DateUpdated = DateTime.Now,
+					CreatedBy = "System",
+					UpdatedBy = "System"
+				},
+
+				new Hall
+				{
+					Id = 2,
+					Name = "1",
+					ScreeningQuality = "DOLBY ATMOS",
+					Capacity = 90,
+					CinemaBranchId = 2,
+					DateCreated = DateTime.Now,
+					DateUpdated = DateTime.Now,
+					CreatedBy = "System",
+					UpdatedBy = "System"
+				},
+
+				new Hall
+				{
+					Id = 3,
+					Name = "1",
+					ScreeningQuality = "HD",
+					Capacity = 80,
+					CinemaBranchId = 3,
+					DateCreated = DateTime.Now,
+					DateUpdated = DateTime.Now,
+					CreatedBy = "System",
+					UpdatedBy = "System"
+				}
+			);
+		}
+	}
 }
